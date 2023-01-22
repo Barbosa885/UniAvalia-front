aeae28623d478f5f5016d4f79ef2ac6771600694
Maycon Fabio <m.vinnicyus@gmail.com>
2023-01-22 12:44:52 -0300

    feat: Create Link button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,29 @@ import { ArrowBackRounded, SearchRounded } from '@mui/icons-material'
 import { Button } from '../components/Button'
 import { FloatingButton } from '../components/FloatingButton'
 import { IconButton } from '../components/IconButton'
+import { LinkButton } from '../components/LinkButton'
 
 export default function Home(): JSX.Element {
   return (
     <>
       <h1 className="text-8xl">UniAvalia</h1>
-      <IconButton size="small" color="primary" Icon={ArrowBackRounded} />
-      <IconButton
-        size="large"
-        color="secondary"
-        Icon={ArrowBackRounded}
-        filled
-      />
-      <Button rounded href="/search">
-        Pesquisar
-      </Button>
-      <Button color="secondary">Cadastrar</Button>
+      <div className="">
+        <IconButton size="small" color="primary" Icon={ArrowBackRounded} />
+        <IconButton
+          size="large"
+          color="secondary"
+          Icon={ArrowBackRounded}
+          filled
+        />
+      </div>
+
+      <div className="">
+        <Button rounded href="/search">
+          Pesquisar
+        </Button>
+        <Button color="secondary">Cadastrar</Button>
+      </div>
+      <LinkButton href="/search">Procurar</LinkButton>
       <FloatingButton size="large" Icon={SearchRounded} />
     </>
   )
