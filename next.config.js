d3192ebb3747941d902e11fd16316de7c347f199
Barbosa885 <80699703+Barbosa885@users.noreply.github.com>
2023-01-25 08:45:55 -0300

    fix: PWA configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
+const withPWA = require("next-pwa")({
+    disable: process.env.NODE_ENV != 'production', 
     dest: "public",
     register: true,
     skipWaiting: true,
-  },
+    sw: '/sw.js',
 });
+
+const nextConfig = withPWA({
+  reactStricMode: true,
+})
+
+module.exports = nextConfig;
