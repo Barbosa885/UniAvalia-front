5cbda830f0456f8bdbfcfa03cb5207194743ff4f
Arthur Carneiro <arthurcarneiro@MacBook-Air-de-Arthur.local>
2023-01-26 16:11:47 -0300

    Card do resultado da pesquisa pronto

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-    disable: process.env.NODE_ENV != 'production', 
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    sw: '/sw.js',
-});
+const withPWA = require('next-pwa')({
+  disable: process.env.NODE_ENV != 'production',
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  sw: '/sw.js',
+})
 
 const nextConfig = withPWA({
   reactStricMode: true,
+  images: {
+    domains: ['www.provafacilnaweb.com.br'],
+  },
 })
 
-module.exports = nextConfig;
+module.exports = nextConfig
