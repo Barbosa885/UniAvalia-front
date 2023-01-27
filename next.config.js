82c48503c95f1db6902ff263615b7e3fc3d97b14
Barbosa885 <80699703+Barbosa885@users.noreply.github.com>
2023-01-27 11:12:47 -0300

    Adds workbox-webpack-plugin depedencie

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  scope: '/',
+
 })
 
 module.exports = withPWA()
