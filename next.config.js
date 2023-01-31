f2768ddea9c6d2a3ebd5a08b44a94e10302857c1
Barbosa885 <gustavosports7@gmail.com>
2023-01-30 23:13:29 -0300

    Fixes PWA

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-const runtimeCaching = [
-  {
-    urlPattern: /^https?.*/,
-    handler: 'NetworkFirst',
-    options: {
-      cacheName: 'offlineCache',
-      expiration: {
-        maxEntries: 200,
-      },
-    },
-  },
-]
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
+const withPWA = require('next-pwa')({
+  dest: 'public',
 })
+
+module.exports = withPWA();
 
