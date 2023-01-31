9b1106053249c9fb5dc0487d1de3b022df006b92
Barbosa885 <gustavosports7@gmail.com>
2023-01-30 23:37:11 -0300

    fix: PWA is now working fine

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
+})
+
+module.exports = withPWA({
   images: {
     domains: ['www.provafacilweb.com.br'],
   },
-})
-
-module.exports = withPWA();
+});
